b2e299716a51f65db6e661167828377334ccb5ea
Bestie03 <73836392+Bestie03@users.noreply.github.com>
2021-01-25 18:28:47 +0000

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Spin_Button = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(143, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(275, 55)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Slot Machine"
+        '
+        'Spin_Button
+        '
+        Me.Spin_Button.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Spin_Button.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Spin_Button.Location = New System.Drawing.Point(74, 373)
+        Me.Spin_Button.Name = "Spin_Button"
+        Me.Spin_Button.Size = New System.Drawing.Size(177, 44)
+        Me.Spin_Button.TabIndex = 1
+        Me.Spin_Button.Text = "Spin!"
+        Me.Spin_Button.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button1.Location = New System.Drawing.Point(325, 373)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(187, 44)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Lock in Bet Amount"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(43, 286)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(216, 29)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Input credit amount"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InfoText
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.Location = New System.Drawing.Point(265, 286)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(175, 30)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.DarkBlue
+        Me.Label3.Font = New System.Drawing.Font("MS Reference Sans Serif", 13.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(100, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(358, 29)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Welcome to Blipz And Chitz!!"
+        '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.PictureBox1.Image = Global.SlotMachine.My.Resources.Resources.cherry
+        Me.PictureBox1.Location = New System.Drawing.Point(74, 131)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(124, 109)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PictureBox2.Image = Global.SlotMachine.My.Resources.Resources._7
+        Me.PictureBox2.Location = New System.Drawing.Point(230, 131)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(124, 109)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PictureBox3.Image = Global.SlotMachine.My.Resources.Resources.cherry
+        Me.PictureBox3.Location = New System.Drawing.Point(388, 131)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(124, 109)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(566, 450)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Spin_Button)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Blips and Chitz Slot Machine"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Spin_Button As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,139 @@
+﻿Public Class Form1
+    Dim amount, x, a, b, c As Integer
+
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        amount = Val(TextBox1.Text)
+        If amount > 0 Then
+            Button1.Visible = False
+        End If
+    End Sub
+
+    Sub spin()
+
+        If amount < 1 Then
+            Label3.Text = "Insufficient betting funds!"
+        Else
+
+            x = x + 10
+            a = 3 + Int(Rnd() * 3)
+            b = 3 + Int(Rnd() * 3)
+            c = 3 + Int(Rnd() * 3)
+
+            Label3.Text = "Good Luck!"
+
+
+            If a = 3 Then
+                PictureBox1.Image = Image.FromFile("C:\Users\cian2\Downloads\7.jpg")
+            End If
+            If a = 4 Then
+                PictureBox1.Image = Image.FromFile("C:\Users\cian2\Downloads\bb.jpg")
+            End If
+
+            If a = 5 Then
+                PictureBox1.Image = Image.FromFile("C:\Users\cian2\Downloads\cherry.jpg")
+            End If
+
+            If b = 3 Then
+                PictureBox2.Image = Image.FromFile("C:\Users\cian2\Downloads\7.jpg")
+            End If
+
+            If b = 4 Then
+                PictureBox2.Image = Image.FromFile("C:\Users\cian2\Downloads\bb.jpg")
+            End If
+            If b = 5 Then
+                PictureBox2.Image = Image.FromFile("C:\Users\cian2\Downloads\cherry.jpg")
+            End If
+
+
+            If c = 3 Then
+                PictureBox3.Image = Image.FromFile("C:\Users\cian2\Downloads\7.jpg")
+            End If
+
+            If c = 4 Then
+                PictureBox3.Image = Image.FromFile("C:\Users\cian2\Downloads\bb.jpg")
+            End If
+
+            If c = 5 Then
+                PictureBox3.Image = Image.FromFile("C:\Users\cian2\Downloads\cherry.jpg")
+            End If
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If x < 500 Then
+            spin()
+        Else
+            Timer1.Enabled = False
+
+            Label3.ImageAlign = 2
+
+            If (a = 4 And b = 4 And c = 4) Then
+                Label3.Text = " You win 20 dollars"
+                amount = amount + 20
+                TextBox1.Text = amount
+
+
+            ElseIf (a = 3 And b = 3 And c = 3) Then
+                Label3.Text = " You win 30 dollars"
+                amount = amount + 30
+                TextBox1.Text = amount
+
+
+
+
+            ElseIf (a = 5 And b = 5 And c = 5) Then
+
+                Label3.Text = " Congratulation! Jackpot!!! You win 200 dollars!"
+                amount = amount + 200
+                TextBox1.Text = amount
+
+
+            Else
+                Label3.Text = " Too bad, you lost 50 dollars"
+                amount = amount - 50
+                TextBox1.Text = amount
+            End If
+
+            If amount < 1 Then
+                Label3.Text = " Uh Oh Spaghettios! you're bankrupt!"
+                TextBox1.Text = Str$(amount)
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label3.Text = " Welcome to Blips and Chitz!"
+
+    End Sub
+
+    Private Sub Spin_Button_Click(sender As Object, e As EventArgs) Handles Spin_Button.Click
+        Timer1.Enabled = True
+
+        x = 0
+        Label2.Text = "Your Credits"
+
+    End Sub
+End Class
+
